ab821fccfb437a5b80c6ca8ff45fc8432b554a34
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-08-03 15:35:24 -0600

    prune all images not associated with a running container instead of rmi

--- update_data_volumes.py
+++ update_data_volumes.py
@@ -226,8 +226,8 @@ def create_data_volumes(volumes_to_create, search_url, data_repo,
             is_ok = False
             continue
 
-        # remove image to free disk space
-        cmd = f'docker rmi dtcenter/{data_repo}:{docker_tag}'
+        # prune docker images to free disk space
+        cmd = f'docker image prune -af'
         if not run_docker_command(cmd):
             is_ok = False
             continue
